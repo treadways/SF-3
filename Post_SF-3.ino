7cdc77b5394251e9c1bb4da14b951893be27c536
Shane Treadway <shanetreadway@Shanes-MacBook-Pro.local>
2017-11-05 23:08:41 -0500

    Final with annotiation

--- Post_SF-3.ino
+++ Post_SF-3.ino
@@ -1,16 +1,16 @@
 
-const int led = 7;      //set up pins
+const int led = 7;      //give pins names
 const int buzzer = 11;
 const int sensor = 0;
 const int button = 4;
 
-int lightLevel, buttonState; 
+int lightLevel, buttonState; //create variables
 int alarmStatus = 0;
 
 
-void setup()
+void setup()   //set up pins
 {
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);  
   pinMode(buzzer, OUTPUT);
   pinMode(button, INPUT);
   
@@ -18,33 +18,33 @@ void setup()
 
 void loop()
 {
-  lightLevel = analogRead(sensor);
-  lightLevel = map(lightLevel, 420, 850, 0, 100);
-  lightLevel = constrain(lightLevel, 0, 100);
+  lightLevel = analogRead(sensor); //read sensor to see if there's light
+  lightLevel = map(lightLevel, 420, 850, 0, 100);  //put sensor data on a scale between 0-100
+  lightLevel = constrain(lightLevel, 0, 100); //only use data in range 0-100
 
-  if (lightLevel > 15)
+  if (lightLevel > 50) //higher light level than 0 to only be sesitive to light directly near it turned on (for the purpose of this assignment)
   {
-    alarmStatus = 1;
+    alarmStatus = 1; //create a variable to initiate while loop
   }
 
-  while (alarmStatus == 1)
+  while (alarmStatus == 1) //start loop if sensor recieved light
   {
-   alarm();
+   alarm(); //start alarm function
 
-   buttonState = digitalRead(button);
+   buttonState = digitalRead(button); //read button signal
    
-   if (buttonState == HIGH)
+   if (buttonState == HIGH) //if button is pressed start if statement
    {
-    reset();
+    reset(); //reset function
    }
   }
 }
 
-void alarm()
+void alarm() //this function turns on the alarm (both the buzzer and lights)
 {
-   digitalWrite(led, HIGH);
+   digitalWrite(led, HIGH); //turn on lights
    
-   tone(buzzer, 200, 10);
+   tone(buzzer, 200, 10); //my own obnoxious noise alarm
    delay(10);
    tone(buzzer, 400, 10);
    delay(10);
@@ -52,12 +52,12 @@ void alarm()
    delay(10);
 }   
 
-void reset()
+void reset() //this function resets the buzzer and lights
 {
-    digitalWrite(led, LOW);
-    tone(buzzer, 0, 10);
-    delay(5000);
-    alarmStatus = 0;
+    digitalWrite(led, LOW); //turn off lights
+    tone(buzzer, 0, 10); //turn off buzzer
+    alarmStatus = 0; //put status back to 0
+    delay(5000); //wait five seconds before letting sensor recieve data
 }
 
 
